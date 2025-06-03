cd3c9ec50c0bcd184735e206bb59861f2fba3519
ehsan <m.ehsan5831@gmail.com>
2025-06-03 13:33:38 +0330

    new Branch added

--- Zombie.java
+++ Zombie.java
@@ -1,2 +1,16 @@
 public abstract class Zombie {
+    private int speed;
+    private int hp;
+
+    public Zombie(int speed, int hp) {
+        this.speed = speed;
+        this.hp = hp;
+    }
+
+    public int getHp(){
+        return hp;
+    }
+    public int getSpeed() {
+        return speed;
+    }
 }
