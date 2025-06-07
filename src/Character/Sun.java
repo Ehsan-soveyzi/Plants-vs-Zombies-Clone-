95deb9bd5ce7ba022f6fe6a01b6eb3ea14ae41f0
saba <sabaahrbi91@gmail.com>
2025-06-07 23:32:24 +0330

    sun and sunflower

---
+++ src/Character/Sun.java
@@ -0,0 +1,65 @@
+package Character;
+
+import javafx.application.Platform;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class Sun {
+    double x;
+    double y;
+    boolean clicked = false;
+    ImageView imageView = new ImageView(new Image("images/sun.png"));
+    Sun(){ // random in map
+        sunMovement();
+        sunCollector();
+    }
+    Sun(double x, double y) {
+        this.x = x;
+        this.y = y;
+        imageView.setX(x);
+        imageView.setY(y);
+        // imageview should add as children
+        sunCollector();
+    }
+    public void sunCollector(){
+        imageView.setOnMouseClicked(event -> {
+            if(clicked) {
+                removeSun();
+            }
+            else clicked = true;
+        });
+
+        new Thread(()->{
+            try {
+                Thread.sleep(3000);
+                if(clicked){
+                    addSun();
+                    removeSun();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (!clicked) {
+                Platform.runLater(() -> {
+                    removeSun();
+                });
+            }
+        }).start();
+    }
+    private void sunMovement(){
+
+    }
+
+    public void addSun(){
+//        map.setSun(map.getSun() + 1);
+    }
+    public void removeSun(){
+//        imageView.setOpacity(0);
+//        if (imageView.getParent() != null) {
+//            ((Pane) imageView.getParent()).getChildren().remove(imageView);
+//        }
+    }
+
+
+}
--- src/Character/SunFlower.java
+++ src/Character/SunFlower.java
@@ -1,19 +1,30 @@
 package Character;
 
+import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.util.Duration;
 
 public class SunFlower extends Plant{
+
     Timeline sunGenerationTime;
+
     SunFlower(int cost, int hp, double x, double y) {
         super(cost, hp, x, y);
-    }
-    public void addSun(){
+        super.setX(x);
+        super.setY(y);
+
+        sunGenerationTime = new Timeline(new KeyFrame(Duration.seconds(5), event -> {
+            if (!isDead()) {
+                Sun sun = new Sun(super.getX(), super.getY());
+            }
+            else {
+                sunGenerationTime.stop();
+            }
+        }));
+
+        sunGenerationTime.setCycleCount(Timeline.INDEFINITE);
+        sunGenerationTime.play();
 
     }
-    public void removeSun(){
 
-    }
-    public void caller(){
-
-    }
 }
