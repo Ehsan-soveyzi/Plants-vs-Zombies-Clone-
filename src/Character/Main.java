b47743e850671ddb67424cd102ab70b26d288aeb
ehsan <m.ehsan5831@gmail.com>
2025-06-09 13:54:58 +0330

    some change!

--- src/Character/Main.java
+++
@@ -1,9 +0,0 @@
-package Character;
-
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Welcome to Plants vs Zombies");
-        System.out.println("Welcome to Plants vs Zombies2");
-        System.out.println("Welcome to Plants vs Zombies3");
-    }
-}
