f81688477d8b70dd59192a6dddf5e496842e3843
saba <sabaahrbi91@gmail.com>
2025-06-09 17:16:49 +0330

    delete p

--- src/Character/KindsOfZombie/ScreenDoorZombie.java
+++ src/Character/KindsOfZombie/ScreenDoorZombie.java
@@ -4,8 +4,8 @@ import javafx.scene.image.Image;
 
 public class ScreenDoorZombie extends Zombie {
     private final static String screenDoorImageAddress = "/Character/screenDoor.png";
-    public ScreenDoorZombie(int hp, double speed, double eatingSpeed, int row, double x, double y) {
-        super(hp, speed, eatingSpeed, row, x, y, new Image(screenDoorImageAddress));
+    public ScreenDoorZombie(int row) {
+        super(10, 1, 1, row,  new Image(screenDoorImageAddress));
     }
     @Override
     public void updateImageSituation(){
