f20864adb17ff8d9da00e002ba18fd840a02378c
saba <sabaahrbi91@gmail.com>
2025-06-09 13:09:08 +0330

    copy play animation

--- src/Character/PeaShooter.java
+++ src/Character/PeaShooter.java
@@ -1,6 +1,12 @@
 package Character;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+import java.util.Objects;
 
 public class PeaShooter extends PeaPlant {
 
@@ -15,6 +21,22 @@ public class PeaShooter extends PeaPlant {
     public void shoot() {
         Bullet normalBullet = new NormalBullet(getX(),getY(),getRow(), 1, 0.5);
 
+    }
+    public void playAnimation() {
+        Image[] frames = new Image[12];
+        for(int i = 0;i < 12;i++){
+            frames[i] = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/resources/graphics/Plants/Peashooter/Peashooter_" + i + ".png")));
+        }
+        final int[] index = {0};
+        ImageView peaShooter = getImageView();
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(100), e ->{
+//            this.update(0.1);
+            peaShooter.setImage(frames[index[0]]);
+            index[0] = (index[0] + 1) % frames.length;
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
     }
 
 
--- src/Character/Regular.java
+++ src/Character/Regular.java
@@ -1,11 +1,15 @@
 package Character;
 
+import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.util.Duration;
 
- public class Regular extends Zombie {
+import java.util.Objects;
+
+public class Regular extends Zombie {
     private final static String regularImageAddress = "/Character/regular.png";
     // or have a image field in parent class and
     // static because before making this field the super execute
@@ -17,9 +21,30 @@ import javafx.util.Duration;
 
     @Override
     protected void playWalkingAnimation() {
+
         time = new Timeline(new KeyFrame(Duration.millis(1000)));
 
-    }
+            Image[] frames = new Image[22];
+            for (int i = 0; i < 22; i++) {
+                frames[i] = new Image(Objects.requireNonNull(getClass().getResourceAsStream(
+                        "/Images/resources/graphics/Zombies/NormalZombie/Zombie/Zombie_" + i + ".png"
+                )));
+            }
+
+            ImageView zombieView = getImageView();
+
+            final int[] frameIndex = {0};
+
+            Timeline timeline = new Timeline(new KeyFrame(Duration.millis(100), e -> {
+                this.update(0.1);
+                zombieView.setImage(frames[frameIndex[0]]);
+                frameIndex[0] = (frameIndex[0] + 1) % frames.length;
+            }));
+
+            timeline.setCycleCount(Animation.INDEFINITE);
+            timeline.play();
+        }
+
 
     protected void  playEatingAnimation(){
 
