7cd9b554f668045815b7b0c80337a952a5d14c81
Ehsan <m.ehsan5831@gmail.com>
2025-06-18 10:55:36 +0330

    mainMenu created!

--- src/ApplyGraphics/GameMain.java
+++ src/ApplyGraphics/GameMain.java
@@ -1,15 +1,17 @@
-package Map;
+package ApplyGraphics;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
-public class MapGrafic extends Application {
+public class GameMain extends Application {
 
     public static void main(String[] args) {
         launch(args);
@@ -17,13 +19,19 @@ public class MapGrafic extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MapFXML.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainMenu.fxml"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
+        MainMenuController.stage = primaryStage;
         primaryStage.setScene(scene);
         primaryStage.setTitle("Map");
         primaryStage.setFullScreenExitHint("");
         primaryStage.setFullScreen(true);
+        //apply fadeTransition!
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), root);
+        fadeIn.setFromValue(0.0);
+        fadeIn.setToValue(1.0);
+        fadeIn.play();
         primaryStage.show();
     }
 }
--- src/ApplyGraphics/MainMenu.java
+++
@@ -1,4 +0,0 @@
-package ApplyGraphics;
-
-public class MainMenu {
-}
