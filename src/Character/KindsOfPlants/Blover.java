5604b97eea94e0d7ef7018a58d6966abca46af23
Ehsan <m.ehsan5831@gmail.com>
2025-06-29 19:13:58 +0330

    fog plants added

---
+++ src/Character/KindsOfPlants/Blover.java
@@ -0,0 +1,60 @@
+package Character.KindsOfPlants;
+
+import Map.GameMap;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+
+import java.io.Serializable;
+
+public class Blover extends Plant implements Serializable {
+    private static final String bloverImageAddress = "/Images/resources/graphics/Plants/Blover/blover.gif";
+    private static final String bloverCardImageAddress = "/Images/resources/graphics/Cards/blover.jpg";
+    public static int cooldown = 7;
+    public static boolean isReady = true;
+    public static Timeline cooldownTimeline;
+
+    public Blover() {
+        super(100,1000,new Image(bloverImageAddress),new Image(bloverCardImageAddress));
+    }
+
+    public void removeFog(){
+        for(int i = 0;i < 5;i++){
+            for(int j = 5;j < 9;j++){
+                GameMap.getInstance().setFoged(i,j,false);
+            }
+        }
+    }
+    public static void startCooldown() {
+        isReady = false;
+        cooldownTimeline = new Timeline(new KeyFrame(Duration.seconds(1),event -> {
+            cooldown--;
+            if(cooldown == 0) {
+                cooldown = 7;
+                isReady = true;
+                cooldownTimeline.stop();
+            }
+        }));
+        cooldownTimeline.setCycleCount(7);
+        cooldownTimeline.play();
+    }
+
+
+    @Override
+    public void updateImageSituation(Pane pane) {
+        startCooldown();
+        removeFog();
+        timeline = new Timeline(new KeyFrame(Duration.seconds(5),event -> {
+        }));
+        timeline.setCycleCount(1);
+        timeline.play();
+        timeline.setOnFinished(event -> {
+            GameMap.getInstance().initializeFog();
+            ((Pane)getImageView().getParent()).getChildren().remove(getImageView());
+            die();
+        });
+
+    }
+}
---
+++ src/Character/KindsOfPlants/Plantern.java
@@ -0,0 +1,53 @@
+package Character.KindsOfPlants;
+
+import Map.GameMap;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+
+public class Plantern extends Plant {
+    private static final String planternImageAddress = "/Images/resources/graphics/Plants/Plantern/Plantern.gif";
+    private static final String planternCardImageAddress = "/Images/resources/graphics/Cards/plantern.jpg";
+    public static int cooldown = 7;
+    public static boolean isReady = true;
+    public static Timeline cooldownTimeline;
+
+
+    public Plantern() {
+        super(25,5,new Image(planternImageAddress),new Image(planternCardImageAddress));
+        //just for handling the size of imageview
+        getImageView().setLayoutX(getImageView().getLayoutX() - 80);
+        getImageView().setLayoutY(getImageView().getLayoutY() - 80);
+    }
+
+    public static void startCooldown() {
+        isReady = false;
+        cooldownTimeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            cooldown--;
+            if(cooldown == 0) {
+                cooldown = 7;
+                isReady = true;
+                cooldownTimeline.stop();
+            }
+        }));
+        cooldownTimeline.setCycleCount(7);
+        cooldownTimeline.play();
+    }
+
+    public void setFog(boolean fog) {
+        for(int i = getRow() - 1;i < getRow() + 2; i++) {
+            for(int j = getCol() - 1;j < getCol() + 2; j++) {
+                if(i < 0 || i > 4 || j < 0 || j > 8)break;
+                GameMap.getInstance().setFoged(i,j,fog);
+            }
+        }
+    }
+
+    @Override
+    public void updateImageSituation(Pane pane) {
+        startCooldown();
+        setFog(false);
+    }
+}
